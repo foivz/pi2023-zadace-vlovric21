3dc17bad3ecb3f4815e9ca068fcb62ee699c4ad1
ViktorL02 <92307949+ViktorL02@users.noreply.github.com>
2023-05-24 09:33:47 +0200

    Implementirana prijava sa hardcoded podatcima

--- Software/HelpDesk/HelpDesk/frmLogin.cs
+++ Software/HelpDesk/HelpDesk/frmLogin.cs
@@ -12,6 +12,8 @@ namespace HelpDesk
 {
     public partial class frmLogin : Form
     {
+        string username = "test";
+        string password = "test";
         public frmLogin()
         {
             InitializeComponent();
@@ -25,6 +27,26 @@ namespace HelpDesk
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtUsername.Text == "")
+            {
+                MessageBox.Show("Unesite korisničko ime!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }else if(txtPassword.Text == "")
+            {
+                MessageBox.Show("Unesite lozinku!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else
+            {
+                if(txtUsername.Text == username && txtPassword.Text == password)
+                {
+                    MessageBox.Show("Uspješna prijava!", "Prijavljeni ste", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else
+                {
+                    MessageBox.Show("Neispravni podatci!", "Neuspješna prijava", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
