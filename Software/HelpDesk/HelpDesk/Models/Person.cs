be7098e61356b65051f4bc15664afac9a56913da
ViktorL02 <92307949+ViktorL02@users.noreply.github.com>
2023-05-24 14:22:30 +0200

    Dodao klase za osobu, podnositelja i zahtjev

---
+++ Software/HelpDesk/HelpDesk/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} {LastName}";
+        }
+    }
+}
---
+++ Software/HelpDesk/HelpDesk/Models/Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Models
+{
+    public class Request
+    {
+        //fali redni broj al to cu kasnije
+        public int Username { get; set; }
+        public DateTime Time { get; set; }
+        public string Description { get; set; }
+        public string Undertaken { get; set; }
+        public string Comment { get; set; }
+        public string Status { get; set; }
+    }
+}
